d1f0953930ac05a36b75c20adc791cdf053cad80
nikzor <100807274+nikzor@users.noreply.github.com>
2022-06-18 14:51:19 +0300

    Auth bug fixes

--- lib/screens/mainscreen.dart
+++ lib/screens/mainscreen.dart
@@ -12,32 +12,33 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Center(
           child: Column(
             children: [
               Container(
-                margin: EdgeInsets.only(top: 300),
+                margin: const EdgeInsets.only(top: 300),
                 height: 54,
                 width: 165,
-                child: Text(
+                child: const Text(
                   'Taurist',
                   style: TextStyle(fontSize: 46, fontFamily: 'Inter'),
                 ),
               ),
               Container(
-                margin: EdgeInsets.only(top: 250),
+                margin: const EdgeInsets.only(top: 250),
                 decoration: BoxDecoration(
-                  gradient: const LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    stops: [0.0, 1.0],
-                    colors: [
-                      Color(0xff01cd8d),
-                      Color(0xff93edc7),
-                    ],
-                  ),
-                  color: Colors.white,
+                  // gradient: const LinearGradient(
+                  //   begin: Alignment.topLeft,
+                  //   end: Alignment.bottomRight,
+                  //   stops: [0.0, 1.0],
+                  //   colors: [
+                  //     Color(0xff01cd8d),
+                  //     Color(0xff93edc7),
+                  //   ],
+                  // ),
+                  color: const Color.fromRGBO(44, 83, 72, 1),
                   borderRadius: BorderRadius.circular(16),
                 ),
                 child: ElevatedButton(
@@ -53,12 +54,13 @@ class _MainPageState extends State<MainPage> {
                     shadowColor: MaterialStateProperty.all(Colors.transparent),
                   ),
                   onPressed: () {
+                    //Navigator.popUntil(context, ModalRoute.withName('/login'));
                     Navigator.pushNamed(
                       context,
                       '/login',
                     );
                   },
-                  child: Padding(
+                  child: const Padding(
                     padding: EdgeInsets.only(
                       top: 10,
                       bottom: 10,
@@ -74,52 +76,52 @@ class _MainPageState extends State<MainPage> {
                   ),
                 ),
               ),
-              Container(
-                margin: EdgeInsets.only(top: 10),
-                decoration: BoxDecoration(
-                  /*gradient: const LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    stops: [0.0, 1.0],
-                    colors: [
-                      Color(0xff01cd8d),
-                      Color(0xff93edc7),
-                    ],
-                  ),*/
-                  color: Color.fromRGBO(44, 83, 72, 1),
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20.0),
-                      ),
-                    ),
-                    minimumSize: MaterialStateProperty.all(const Size(260, 40)),
-                    backgroundColor:
-                        MaterialStateProperty.all(Colors.transparent),
-                    shadowColor: MaterialStateProperty.all(Colors.transparent),
-                  ),
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/signin');
-                  },
-                  child: Padding(
-                    padding: EdgeInsets.only(
-                      top: 10,
-                      bottom: 10,
-                    ),
-                    child: Text(
-                      'Sign Up',
-                      style: TextStyle(
-                        fontFamily: 'Inter',
-                        fontSize: 16,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
+              // Container(
+              //   margin: const EdgeInsets.only(top: 10),
+              //   decoration: BoxDecoration(
+              //     /*gradient: const LinearGradient(
+              //       begin: Alignment.topLeft,
+              //       end: Alignment.bottomRight,
+              //       stops: [0.0, 1.0],
+              //       colors: [
+              //         Color(0xff01cd8d),
+              //         Color(0xff93edc7),
+              //       ],
+              //     ),*/
+              //     color: Color.fromRGBO(44, 83, 72, 1),
+              //     borderRadius: BorderRadius.circular(16),
+              //   ),
+              //   child: ElevatedButton(
+              //     style: ButtonStyle(
+              //       shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              //         RoundedRectangleBorder(
+              //           borderRadius: BorderRadius.circular(20.0),
+              //         ),
+              //       ),
+              //       minimumSize: MaterialStateProperty.all(const Size(260, 40)),
+              //       backgroundColor:
+              //           MaterialStateProperty.all(Colors.transparent),
+              //       shadowColor: MaterialStateProperty.all(Colors.transparent),
+              //     ),
+              //     onPressed: () {
+              //       Navigator.pushNamed(context, '/signin');
+              //     },
+              //     child: const Padding(
+              //       padding: EdgeInsets.only(
+              //         top: 10,
+              //         bottom: 10,
+              //       ),
+              //       child: Text(
+              //         'Sign Up',
+              //         style: TextStyle(
+              //           fontFamily: 'Inter',
+              //           fontSize: 16,
+              //           color: Colors.white,
+              //         ),
+              //       ),
+              //     ),
+              //   ),
+              // ),
             ],
           ),
         ),
---
+++ lib/screens/profilescreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            child: Text('This is main page'),
+          )
+        ],
+      ),
+    );
+  }
+}
