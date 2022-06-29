ea2ddf96e86de7bb5d773d651e37001cabe679d8
Markov Andrei <me@markovav.ru>
2022-06-29 15:57:11 +0300

    Import routes

--- lib/screens/signIn/main_screen.dart
+++ lib/screens/signIn/main_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:taurist/controllers/authorization_controller.dart';
+import 'package:taurist/routes.dart';
 
 class SignInPage extends StatefulWidget {
   const SignInPage({Key? key}) : super(key: key);
