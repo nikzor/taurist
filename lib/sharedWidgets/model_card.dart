b74459c62b944f978d8dd863ce7e41530fadd82a
nikzor <100807274+nikzor@users.noreply.github.com>
2022-07-05 15:28:29 +0300

    Design fix

--- lib/sharedWidgets/model_card.dart
+++ lib/sharedWidgets/model_card.dart
@@ -2,12 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:taurist/data/route_model.dart';
 
 Widget getModelCardWidget(RouteModel model) {
-  return Container(
+  return Card(
     margin: const EdgeInsets.all(8.0),
-    decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: Colors.white,
-        border: Border.all(color: Colors.black)),
     child: Padding(
       padding: const EdgeInsets.all(10.0),
       child: Row(
@@ -16,14 +12,10 @@ Widget getModelCardWidget(RouteModel model) {
           Container(
             padding: const EdgeInsets.all(10.0),
             margin: const EdgeInsets.symmetric(horizontal: 10.0),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(50),
-              border: Border.all(color: Colors.black),
-            ),
             child: Icon(
-              color: const Color.fromRGBO(44, 83, 72, 1),
+              color: Color.fromRGBO(44, 83, 72, 1),
               Icons.bike_scooter,
-              size: 30,
+              size: 36,
             ),
           ),
           Flexible(
@@ -32,8 +24,8 @@ Widget getModelCardWidget(RouteModel model) {
             child: Text(
               model.title,
               style: const TextStyle(
-                fontSize: 16.0,
-                fontWeight: FontWeight.bold,
+                fontSize: 20.0,
+                fontWeight: FontWeight.w600,
               ),
             ),
           ),
@@ -46,14 +38,14 @@ Widget getModelCardWidget(RouteModel model) {
                 Text(
                   "${model.distance.toStringAsFixed(2)} km",
                   style: const TextStyle(
-                    fontSize: 12.0,
+                    fontSize: 14.0,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
                 Text(
                   model.durationAsTimeString(),
                   style: const TextStyle(
-                    fontSize: 12.0,
+                    fontSize: 14.0,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
