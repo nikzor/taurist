23b6b07565e36a986685bd2a974b11568cc02144
Markov Andrei <me@markovav.ru>
2022-07-07 19:27:12 +0300

    Fixed location permissions

--- lib/sharedWidgets/model_desc_card.dart
+++ lib/sharedWidgets/model_desc_card.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map/plugin_api.dart';
 import 'package:get/get.dart';
@@ -19,16 +18,18 @@ class LiveLocationPage extends StatefulWidget {
 }
 
 class LiveLocationPageState extends State<LiveLocationPage> {
-  LocationData? _currentLocation;
+  final Location _locationService = Location();
+  LocationData? _locationData;
   late final MapController _mapController;
 
   // bool _liveUpdate = false;
-  bool _permission = false;
 
-  // var interActiveFlags = InteractiveFlag.all;
-  var interActiveFlags = InteractiveFlag.all - InteractiveFlag.drag;
+  bool _serviceEnabled = false;
+  PermissionStatus _permissionGranted = PermissionStatus.denied;
 
-  final Location _locationService = Location();
+  /// uncomment if better scrollView experience is required
+  // var interActiveFlags = InteractiveFlag.all - InteractiveFlag.drag;
+  var interActiveFlags = InteractiveFlag.all;
 
   @override
   void initState() {
@@ -38,54 +39,83 @@ class LiveLocationPageState extends State<LiveLocationPage> {
   }
 
   void initLocationService() async {
-    await _locationService.changeSettings(
-      accuracy: LocationAccuracy.high,
-      interval: 1000,
-    );
-
     LocationData? location;
-    bool serviceEnabled;
-    bool serviceRequestResult;
 
-    try {
-      serviceEnabled = await _locationService.serviceEnabled();
+    _serviceEnabled = await _locationService.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await _locationService.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
 
-      if (serviceEnabled) {
-        var permission = await _locationService.requestPermission();
-        _permission = permission == PermissionStatus.granted;
+    _permissionGranted = await _locationService.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await _locationService.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
 
-        if (_permission) {
-          location = await _locationService.getLocation();
-          _currentLocation = location;
-          _locationService.onLocationChanged.listen(
-            (LocationData result) async {
-              if (mounted) {
-                setState(
-                  () {
-                    _currentLocation = result;
-                    // if (_liveUpdate) {
-                    //   _mapController.move(
-                    //       LatLng(_currentLocation!.latitude!,
-                    //           _currentLocation!.longitude!),
-                    //       _mapController.zoom);
-                    // }
-                  },
-                );
-              }
+    location = await _locationService.getLocation();
+    _locationData = location;
+    _locationService.onLocationChanged.listen(
+          (LocationData result) async {
+        if (mounted) {
+          setState(
+                () {
+              _locationData = result;
+              // if (_liveUpdate) {
+              //   _mapController.move(
+              //       LatLng(_currentLocation!.latitude!,
+              //           _currentLocation!.longitude!),
+              //       _mapController.zoom);
+              // }
             },
           );
         }
-      } else {
-        serviceRequestResult = await _locationService.requestService();
-        if (serviceRequestResult) {
-          initLocationService();
-          return;
-        }
-      }
-    } on PlatformException catch (e) {
-      debugPrint(e.toString());
-      location = null;
-    }
+      },
+    );
+
+    // try {
+    //   _serviceEnabled = await _locationService.serviceEnabled();
+    //
+    //   if (!_serviceEnabled) {
+    //     var permission = await _locationService.requestPermission();
+    //     _permission = permission == PermissionStatus.granted;
+    //
+    //     if (_permission) {
+    //       location = await _locationService.getLocation();
+    //       _locationData = location;
+    //       _locationService.onLocationChanged.listen(
+    //         (LocationData result) async {
+    //           if (mounted) {
+    //             setState(
+    //               () {
+    //                 _locationData = result;
+    //                 // if (_liveUpdate) {
+    //                 //   _mapController.move(
+    //                 //       LatLng(_currentLocation!.latitude!,
+    //                 //           _currentLocation!.longitude!),
+    //                 //       _mapController.zoom);
+    //                 // }
+    //               },
+    //             );
+    //           }
+    //         },
+    //       );
+    //     }
+    //   } else {
+    //     serviceRequestResult = await _locationService.requestService();
+    //     if (serviceRequestResult) {
+    //       initLocationService();
+    //       return;
+    //     }
+    //   }
+    // } on PlatformException catch (e) {
+    //   debugPrint(e.toString());
+    //   location = null;
+    // }
   }
 
   @override
@@ -94,9 +124,9 @@ class LiveLocationPageState extends State<LiveLocationPage> {
 
     LatLng currentLatLng;
 
-    if (_currentLocation != null) {
+    if (_locationData != null) {
       currentLatLng =
-          LatLng(_currentLocation!.latitude!, _currentLocation!.longitude!);
+          LatLng(_locationData!.latitude!, _locationData!.longitude!);
     } else {
       currentLatLng = LatLng(0, 0);
     }
@@ -116,53 +146,48 @@ class LiveLocationPageState extends State<LiveLocationPage> {
       builder: (context, AsyncSnapshot<String> snapshot) {
         return !snapshot.hasData
             ? const CircularProgressIndicator(
-                semanticsLabel: 'Loading...',
-                color: Color.fromRGBO(44, 83, 72, 1),
-              )
+          semanticsLabel: 'Loading...',
+          color: Color.fromRGBO(44, 83, 72, 1),
+        )
             : FlutterMap(
-                mapController: _mapController,
-                options: MapOptions(
-                  center: extractWaypoints(snapshot.data!).first,
-                  zoom: 17.0,
-                  minZoom: 1.5,
-                  maxZoom: 18.0,
-                  interactiveFlags: interActiveFlags,
+          mapController: _mapController,
+          options: MapOptions(
+            center: extractWaypoints(snapshot.data!).first,
+            zoom: 17.0,
+            minZoom: 1.5,
+            maxZoom: 18.0,
+            interactiveFlags: interActiveFlags,
+          ),
+          layers: [
+            TileLayerOptions(
+              urlTemplate:
+              "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+              subdomains: ['a', 'b', 'c'],
+
+              /// worse performance, but better maps
+              /// retinaMode: true,
+              /// maxZoom: 22,
+              tilesContainerBuilder:
+              Get.isDarkMode ? darkModeTilesContainerBuilder : null,
+            ),
+            PolylineLayerOptions(
+              polylines: [
+                Polyline(
+                  points: extractWaypoints(snapshot.data!),
+                  strokeWidth: 3.5,
+                  color: Colors.purpleAccent,
                 ),
-                layers: [
-                  TileLayerOptions(
-                    urlTemplate:
-                        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                    subdomains: ['a', 'b', 'c'],
-                    // worse performance, but better maps
-                    // retinaMode: true,
-                    // maxZoom: 22,
-                    tilesContainerBuilder:
-                        Get.isDarkMode ? darkModeTilesContainerBuilder : null,
-                  ),
-                  PolylineLayerOptions(
-                    polylines: [
-                      Polyline(
-                        points: extractWaypoints(snapshot.data!),
-                        strokeWidth: 3.5,
-                        color: Colors.purpleAccent,
-                      ),
-                    ],
-                  ),
-                  MarkerLayerOptions(
-                    markers: [
-                      ...extractMarkers(snapshot.data!, context),
-                      locationMarker,
-                    ],
-                    rotate: true,
-                  ),
-                ],
-                // nonRotatedChildren: [
-                //   AttributionWidget.defaultWidget(
-                //     source: 'OpenStreetMap contributors',
-                //     onSourceTapped: null,
-                //   ),
-                // ],
-              );
+              ],
+            ),
+            MarkerLayerOptions(
+              markers: [
+                ...extractMarkers(snapshot.data!, context),
+                locationMarker,
+              ],
+              rotate: true,
+            ),
+          ],
+        );
       },
     );
   }
